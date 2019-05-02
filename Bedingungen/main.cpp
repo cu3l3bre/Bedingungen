bb85d8cda7618d6b02adbe935c645ffa7356c0fb
cu3l3bre <cu3l3bre@googlemail.com>
2019-05-02 13:08:34 +0200

    Projektdateien hinzufügen.

---
+++ Bedingungen/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string EingabePasswort = "";
+	string AktPasswort = "Test";
+
+	cout << "Bedingungn" << endl;
+
+	// Benutzer nach einem Passwort fragen
+	cout << "Bitte geben Sie ein Passwort ein: ";
+	cin >> EingabePasswort;
+
+	// Passwortabfrage
+
+	if (EingabePasswort == AktPasswort)
+	{
+		cout << "Das Passwort ist richtig!" << endl;
+	}
+	else
+	{
+		cout << "Das Passwort ist falsch!" << endl;
+	}
+
+
+
+
+
+
+
+
+
+	system("pause");
+	return 0;
+}
+
